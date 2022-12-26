62d6f660bc4ae46bbc5d2eae104121c3efaab769
ashu-pattanayak <ashutoshpattanayak2013@gmail.com>
2022-12-26 21:40:20 +0000

    codebase

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.12.1"
+    }
+  }
+backend "s3" {
+    bucket = "terraform-ashutosh"
+    key    = "demo/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
